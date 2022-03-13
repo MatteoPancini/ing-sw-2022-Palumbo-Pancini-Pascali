d892a351462c201e6fcc9137fbc7103c391ebd38
SoglianoC <luigi.pascali@mail.polimi.it>
2022-03-13 18:19:09 +0100

    AssistantCard implementation

---
+++ src/main/java/AssistantDeck.java
@@ -0,0 +1,26 @@
+public class AssistantCard {
+    private Wizard type;
+    private OrderValue value;
+    private MotherNatureMoves moves;
+    private CardState state;
+
+    public Wizard getType() {
+        return type;
+    }
+
+    public OrderValue getValue() {
+        return value;
+    }
+
+    public MotherNatureMoves getMoves() {
+        return moves;
+    }
+
+    public CardState getState() {
+        return state;
+    }
+
+    public void setState(CardState state){
+        this.state = state;
+    }
+}
