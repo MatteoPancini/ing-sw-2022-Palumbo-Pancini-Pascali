8239f87ab32186059b0b50d50ff352702afaf08d
SoglianoC <luigi.pascali@mail.polimi.it>
2022-03-13 18:40:58 +0100

    AssistantDeck implementation

---
+++ src/main/java/model/AssistantDeck.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList
+
+package model;
+
+import model.*
+
+public class AssistantDeck {
+    private ArrayList<AssistantCard> deck;
+
+    public ArrayList<AssistantCard> getDeck() {
+        return deck;
+    }
+
+    public void removeAssistant(AssistantCard card){
+
+    }
+
+}
